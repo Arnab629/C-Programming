f8acb69b80eee27e2f34273e07fb5834ee26a1b9
Arnab-MCA-SecA-11- 2024-26 <135867495+Arnab629@users.noreply.github.com>
2025-01-18 21:49:39 +0530

    Add files via upload

---
+++ Assignment 1/16.c
@@ -0,0 +1,15 @@
+// Write a C program to check whether a number is even or odd.
+
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("**Odd-Even**\n");
+ printf("Name: Arnab Mondal, Class: MCA1A, Roll: 11\n");
+ printf("Enter a number :");
+ scanf("%d",&n);
+ if(n%2==0)
+    printf("Even Number\n");
+  else
+    printf("Odd Number\n");
+}
---
+++ Assignment 1/9.c
@@ -0,0 +1,9 @@
+// Write a program to print your name without using any semicolons in the program.
+
+#include<stdio.h>
+int main()
+{
+     printf("\n**Print Name Without Any Semicolon**\n");
+     printf("Name: Arnab Mondal, Class: MCA1A, Roll: 11\n");
+     if(printf("My name is Arnab Mondal\n")){}
+}
